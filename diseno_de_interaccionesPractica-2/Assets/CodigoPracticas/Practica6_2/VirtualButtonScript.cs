cf0b921e8e2a92cc56d962c4943e89c47b6762de
Felix Quintero <felix.quintero@uabc.edu.mx>
2021-04-09 13:51:35 -0700

    prac-6--y--6-2

---
+++ diseno_de_interaccionesPractica-2/Assets/CodigoPracticas/Practica6_2/VirtualButtonScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using Vuforia;
+//ps000
+public class VirtualButtonScript : MonoBehaviour, IVirtualButtonEventHandler
+{
+    //public Interactable interactableObject;
+    private VirtualButtonBehaviour virtualButton;
+
+    public Animator animcontroller;
+
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        //interactableObject.Interact();
+        Debug.Log("Se presionó");
+        
+        animcontroller.SetBool("Saludo", true);
+
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Se levantó");
+
+         animcontroller.SetBool("Saludo", false);
+
+    }
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        virtualButton = GetComponent<VirtualButtonBehaviour>();
+    }
+
+    void Start()
+    {
+        if (virtualButton != null)
+        {
+            virtualButton.RegisterEventHandler(this);
+        }
+    }
+}
